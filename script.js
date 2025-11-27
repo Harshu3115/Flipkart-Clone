c6cce5eab277c3b1c236aa415a53a27f153a199f
Harshad Shinde <167699558+Harshu3115@users.noreply.github.com>
2025-11-27 11:05:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var email = prompt("Enter your name :  Note : Do not enter with capital letters and enter without space.");
+console.log("yor entered this name : " + email);
+
+var count = email.length;
+console.log(`Your email id is : ${email+count}@gmail.com`);
